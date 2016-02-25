19aff896a2faea52fa70235f1f423458084c738f
nannen05 <nannen05@gmail.com>
2016-02-24 17:18:46 -0800

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
-
+var browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 
@@ -27,5 +27,6 @@ gulp.task('coffee', function() {
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
