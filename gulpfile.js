e3c14cb63a859509fbbeeb91a1a682f423f88380
nannen05 <nannen05@gmail.com>
2016-02-24 22:25:12 -0800

    added production minified files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
-var compass = require('gulp-compass');
-var browserify = require('gulp-browserify');
-var connect = require('gulp-connect');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat'),
+	compass = require('gulp-compass'),
+	browserify = require('gulp-browserify'),
+	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+ 	minifyHTML = require('gulp-minify-html');
 
 var env,
 	coffeeSources,
@@ -36,7 +39,7 @@ jsSources = [
 
 sassSources = ['components/sass/style.scss'];
 
-htmlSources = [ outputDir + '*html'];
+htmlSources = [ outputDir + '*.html'];
 jsonSources = [ outputDir + 'js/*.json'];
 
 gulp.task('log', function() {
@@ -54,6 +57,7 @@ gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify()))
 		.pipe(gulp.dest( outputDir + 'js'))
 		.pipe(connect.reload())
 });
@@ -71,7 +75,9 @@ gulp.task('compass', function(){
 });
 
 gulp.task('html', function() {
-	gulp.src(htmlSources)
+	gulp.src('builds/development/*.html')
+		.pipe(gulpif( env === 'production', minifyHTML()))
+		.pipe(gulpif( env === 'production', gulp.dest(outputDir)))
 		.pipe(connect.reload())
 });
 
@@ -84,7 +90,7 @@ gulp.task('watch' , function() {
 	gulp.watch(coffeeSources, ['coffee'])
 	gulp.watch(jsSources, ['js'])
 	gulp.watch('components/sass/*.scss', ['compass'])
-	gulp.watch(htmlSources, ['html'])
+	gulp.watch('builds/development/*.html', ['html'])
 	gulp.watch(jsonSources, ['json'])
 })
 
